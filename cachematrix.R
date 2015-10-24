619c62c6de93c40fc1f403332a66f2a683913e42
apsych <apsych@users.noreply.github.com>
2015-10-24 21:47:21 +0300

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -18,29 +18,32 @@
 ## Create a cacheMatrix object for an invertale matrix.
 
 makeCacheMatrix <- function(x = matrix()) {
-  cachedInverse <- NULL
-  set <- function(y) {
-    x <<- y
-    cachedInverse <<- NULL
-  }
-  get <- function() x
-  setInverse <- function(inverse) cachedInverse <<- inverse
-  getInverse <- function() cachedInverse
-  list(set = set, get = get,
-       setInverse = setInverse,
-       getInverse = getInverse)
+        inv <- NULL
+        set <- function(y) {
+                x <<- y
+                inv <<- NULL
+        }
+        get <- function() x
+        setInverse <- function(inverse) inv <<- inverse
+        getInverse <- function() inv
+        list(set = set,
+             get = get,
+             setInverse = setInverse,
+             getInverse = getInverse)
 }
+
 
 ## Return the inverse of an cacheMatrix object
 
 cacheSolve <- function(x, ...) {
-  invFunc <- x$getInverse()
-  if(!is.null(invFunc)) {
-    message("getting cached data")
-    return(invFunc)
-  }
-  data <- x$get()
-  invFunc <- solve(data, ...)
-  x$setInverse(invFunc)
-  invFunc
+        ## Return a matrix that is the inverse of 'x'
+        inv <- x$getInverse()
+        if (!is.null(inv)) {
+                message("getting cached data")
+                return(inv)
+        }
+        mat <- x$get()
+        inv <- solve(mat, ...)
+        x$setInverse(inv)
+        inv
 }
